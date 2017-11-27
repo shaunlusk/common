c553a65ab304d196aed5346b781f734d38140fcb
Shaun Lusk <shaun.lusk@gmail.com>
2017-11-26 19:41:00 -0600

    Unknown state, checking in anyway

---
+++ wrapperGenerator/index2.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var modulePath = process.argv[2];
+var className = process.argv[3];
+var innerMemberName = process.argv[4];
+var wrapperName = process.argv[5];
+var generateWrapper = global.generateWrapper;
+var cls = global[className];
+
+if( typeof generateWrapper === 'undefined' ) {
+  if( has_require ) {
+    generateWrapper = require('./lib.js');
+  }
+  else throw new Error('Requires wrapperGenerator lib.js.');
+}
+
+if( typeof cls === 'undefined' ) {
+  if( has_require ) {
+    cls = require(modulePath)[className];
+  }
+  else throw new Error('Requires module: ' + modulePath);
+}
+
+var wrapper = generateWrapper(cls, innerMemberName, wrapperName);
+
+console.log(wrapper);
